eef346763a163d483dd9a982f39c1dfed9bf774c
Felix <felix.rn.zhou@gmail.com>
2020-03-08 22:06:21 -0400

    add Coordinator protocol

---
+++ macro-tracker/Protocols/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  macro-tracker
+//
+//  Created by Felix on 2020-03-08.
+//  Copyright © 2020 Felix. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    func start()
+}
