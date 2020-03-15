a94bcae4bc241cbe3199567499aa42483503846c
Felix <felix.rn.zhou@gmail.com>
2020-03-14 22:32:15 -0400

    add Core Data handler

---
+++ macro-tracker/MainFlowControllers/ProfileTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileTableViewCell.swift
+//  macro-tracker
+//
+//  Created by Felix on 2020-03-13.
+//  Copyright © 2020 Felix. All rights reserved.
+//
+
+import Foundation
---
+++ macro-tracker/Model/DataCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  DataCalculator.swift
+//  macro-tracker
+//
+//  Created by Felix on 2020-03-13.
+//  Copyright © 2020 Felix. All rights reserved.
+//
+
+import Foundation
